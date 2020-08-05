dcca29a2f3bd7deb36b4525300032d132a72f1d6
Martin Currie <martin.currie@nudgedigital.co.uk>
2020-08-05 21:22:16 +0100

    Used Renamer powershell script to change filenames/code from BaseApi to LBHFSSPublicApi

---
+++ LBHFSSPublicAPI/V1/Gateways/IExampleGateway.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using LBHFSSPublicAPI.V1.Domain;
+
+namespace LBHFSSPublicAPI.V1.Gateways
+{
+    public interface IExampleGateway
+    {
+        Entity GetEntityById(int id);
+
+        List<Entity> GetAll();
+    }
+}
---
+++ LBHFSSPublicAPI/V1/UseCase/Interfaces/IGetAllUseCase.cs
@@ -0,0 +1,9 @@
+using LBHFSSPublicAPI.V1.Boundary.Response;
+
+namespace LBHFSSPublicAPI.V1.UseCase.Interfaces
+{
+    public interface IGetAllUseCase
+    {
+        ResponseObjectList Execute();
+    }
+}
