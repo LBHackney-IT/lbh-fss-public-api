4dd6b6ccec6ed72d1147eae68f7abd9342284dcb
gentex21 <mrpreston_99@yahoo.com>
2020-08-17 14:17:06 +0100

    removed boilerplate code not being used.

--- LBHFSSPublicAPI/V1/UseCase/Interfaces/IGetByIdUseCase.cs
+++
@@ -1,9 +0,0 @@
-using LBHFSSPublicAPI.V1.Boundary.Response;
-
-namespace LBHFSSPublicAPI.V1.UseCase.Interfaces
-{
-    public interface IGetByIdUseCase
-    {
-        //ResponseObject Execute(int id);
-    }
-}
