b929e127917f80ac07884e6d17312c3ba07668ea
gentex21 <mrpreston_99@yahoo.com>
2020-09-03 17:32:51 +0100

    Migration file for the new weight database field.

---
+++ LBHFSSPublicAPI/V1/Infrastructure/Migrations/20200902111601_AddWeightToTaxonomies.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LBHFSSPublicAPI.V1.Infrastructure.Migrations
+{
+    public partial class AddWeightToTaxonomies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "weight",
+                table: "taxonomies",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "weight",
+                table: "taxonomies");
+        }
+    }
+}
