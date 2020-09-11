1c771d8a76a898df4cc8534daf1119e741b0cec9
gentex21 <mrpreston_99@yahoo.com>
2020-09-11 15:26:26 +0100

    added description field to taxonomies table.

--- LBHFSSPublicAPI/V1/Infrastructure/Taxonomy.cs
+++ LBHFSSPublicAPI/V1/Infrastructure/Taxonomy.cs
@@ -9,6 +9,7 @@ namespace LBHFSSPublicAPI.V1.Infrastructure
         public int? ParentId { get; set; }
         public string Vocabulary { get; set; }
         public int Weight { get; set; }
+        public string Description { get; set; }
         public DateTime? CreatedAt { get; set; }
     }
 }
