58b8e7eaae58993b11d38c1b3b39685b17c63ae6
gentex21 <mrpreston_99@yahoo.com>
2020-09-11 15:32:13 +0100

    Updated taxonomy response types to return a single entity or a collection of entities.

--- LBHFSSPublicAPI/V1/Boundary/Response/TaxonomyResponse.cs
+++ LBHFSSPublicAPI/V1/Boundary/Response/TaxonomyResponse.cs
@@ -5,7 +5,12 @@ namespace LBHFSSPublicAPI.V1.Boundary.Response
 {
     public class TaxonomyResponse
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Vocabulary { get; set; }
+        public int Weight { get; set; }
         //Guidance on XML comments and response objects here (https://github.com/LBHackney-IT/lbh-base-api/wiki/Controllers-and-Response-Objects)
-        public List<TaxonomyEntity> Taxonomies { get; set; }
     }
 }
+
---
+++ LBHFSSPublicAPI/V1/Boundary/Response/TaxonomyResponseList.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using LBHFSSPublicAPI.V1.Domain;
+
+namespace LBHFSSPublicAPI.V1.Boundary.Response
+{
+    public class TaxonomyResponseList
+    {
+        public List<TaxonomyResponse> Taxonomies { get; set; }
+    }
+}
+
