cbfb2da3201bef4b0f35201bf0dcc858027ac027
gentex21 <mrpreston_99@yahoo.com>
2020-09-22 12:05:36 +0100

    Added new create methods to the EntityHelpers class to manage the creation of all objects.

--- LBHFSSPublicAPI.Tests/TestHelpers/EntityHelpers.cs
+++ LBHFSSPublicAPI.Tests/TestHelpers/EntityHelpers.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using AutoFixture;
 using LBHFSSPublicAPI.V1.Infrastructure;
 
@@ -8,13 +10,108 @@ namespace LBHFSSPublicAPI.Tests.TestHelpers
         public static Service CreateService()
         {
             var service = Randomm.Build<Service>()
-                .With(s => s.Organization,
-                    Randomm.Build<Organization>()
-                        .With(o => o.ReviewerU, Randomm.Create<User>())
-                        .Create())
-                .With(s => s.Image, Randomm.Create<File>())
+                .Without(s => s.Id)
+                .With(s => s.Organization, CreateOrganization())
+                .With(s => s.Image, CreateFile)
                 .Create();
             return service;
         }
+
+        public static User CreateUser()
+        {
+            return Randomm.Build<User>().Without(u => u.Id).Create();
+        }
+
+        public static File CreateFile()
+        {
+            return Randomm.Build<File>().Without(f => f.Id).Create();
+        }
+
+        public static Role CreateRole()
+        {
+            return Randomm.Build<Role>()
+                .Without(r => r.Id)
+                .Create();
+        }
+
+        public static SynonymGroup CreateSynonymGroup()
+        {
+            return Randomm.Build<SynonymGroup>()
+                .Without(s => s.Id)
+                .Create();
+        }
+
+        public static SynonymWord CreateSynonymWord()
+        {
+            return Randomm.Build<SynonymWord>()
+                .Without(s => s.Id)
+                .With(s => s.Group, CreateSynonymGroup())
+                .Create();
+        }
+        public static Organization CreateOrganization()
+        {
+            var organization = Randomm.Build<Organization>()
+                .Without(o => o.Id)
+                .With(o => o.ReviewerU, CreateUser())
+                .Create();
+            return organization;
+        }
+
+        public static ServiceLocation CreateServiceLocation()
+        {
+            var serviceLocation = Randomm.Build<ServiceLocation>()
+                .Without(sl => sl.Id)
+                .With(sl => sl.Service, CreateService())
+                .Create();
+            return serviceLocation;
+        }
+
+        public static Session CreateSession()
+        {
+            var session = Randomm.Build<Session>()
+                .Without(o => o.Id)
+                .With(o => o.User, CreateUser())
+                .Create();
+            return session;
+        }
+
+        public static Taxonomy CreateTaxonomy()
+        {
+            var taxonomy = Randomm.Build<Taxonomy>()
+                .Without(t => t.Id)
+                .Create();
+            return taxonomy;
+        }
+
+        public static UserOrganization CreateUserOrganization()
+        {
+            var userOrganization = Randomm.Build<UserOrganization>()
+                .Without(uo => uo.Id)
+                .With(uo => uo.User, CreateUser())
+                .With(uo => uo.Organization, CreateOrganization())
+                .Create();
+            return userOrganization;
+        }
+
+        public static ICollection<Taxonomy> CreateTaxonomies(int count = 3)
+        {
+            var taxonomies = new List<Taxonomy>();
+            for (var a = 0; a < count; a++)
+            {
+                taxonomies.Add(CreateTaxonomy());
+            }
+
+            return taxonomies;
+        }
+
+        // public static UserRole CreateUserRole()
+        // {
+        //     var userRole = Randomm.Build<UserRole>()
+        //         .Without(ur => ur.Id)
+        //         .With(ur => ur.IdNavigation, CreateUser())
+        //         .With(ur => ur.Role, CreateRole())
+        //         .Create();
+        //     return userRole;
+        // }
     }
 }
