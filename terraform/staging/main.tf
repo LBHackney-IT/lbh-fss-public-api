4cd5aced24038cc67a18c029e2b23e5077ffcf55
Selwyn Preston <31885748+LBHSPreston@users.noreply.github.com>
2020-10-14 21:03:25 +0100

    Corrected database version

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -62,7 +62,7 @@ module "postgres_db_staging" {
   environment_name = "staging"
   vpc_id = data.aws_vpc.staging_vpc.id
   db_engine = "postgres"
-  db_engine_version = "11.1"
+  db_engine_version = "11.8"
   db_identifier = "fss-public-staging"
   db_instance_class = "db.t2.micro"
   db_name = data.aws_ssm_parameter.fss_public_postgres_database.value
