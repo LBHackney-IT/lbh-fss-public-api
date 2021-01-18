78756fe231aa30d95c39ea7ee22fb7cd3ed1863d
LBHSPreston <selwyn.preston@hackney.gov.uk>
2021-01-18 21:38:19 +0000

    add database migration for new analytics table

---
+++ LBHFSSPublicAPI/V1/Infrastructure/Migrations/20210118213014_AddAnalyticsTable.cs
@@ -0,0 +1,43 @@
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace LBHFSSPublicAPI.V1.Infrastructure.Migrations
+{
+    public partial class AddAnalyticsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "service_analytics",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    service_id = table.Column<int>(nullable: false),
+                    description = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_service_analytics", x => x.id);
+                    table.ForeignKey(
+                        name: "service_analytics_service_id_fkey",
+                        column: x => x.service_id,
+                        principalTable: "services",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_service_analytics_service_id",
+                table: "service_analytics",
+                column: "service_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "service_analytics");
+        }
+    }
+}
