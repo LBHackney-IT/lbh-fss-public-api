4a82296814b0367488064a750240467de4f5010d
NudgeNigelStanhope <nigel.stanhope@nudgedigital.co.uk>
2021-02-22 22:08:38 +0000

    Added two database fields for the CIC update.

---
+++ LBHFSSPublicAPI/V1/Infrastructure/Migrations/20210222214856_AddCicFields.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LBHFSSPublicAPI.V1.Infrastructure.Migrations
+{
+    public partial class AddCicFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "is_registered_charity",
+                table: "organizations",
+                newName: "is_registered_community_interest_company");
+
+            migrationBuilder.RenameColumn(
+                name: "charity_number",
+                table: "organizations",
+                newName: "community_interest_company_number");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "is_registered_community_interest_company",
+                table: "organizations",
+                newName: "is_registered_charity");
+
+            migrationBuilder.RenameColumn(
+                name: "community_interest_company_number",
+                table: "organizations",
+                newName: "charity_number");
+        }
+    }
+}
