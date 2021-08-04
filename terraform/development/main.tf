f4aa7dad0c2c1bf0091573eea95419749c21b737
LBHSPreston <31885748+LBHSPreston@users.noreply.github.com>
2021-08-04 08:54:08 +0100

    remove terraform steps from project.  This should be done separately.

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -64,7 +64,7 @@ module "postgres_db_development" {
   db_engine = "postgres"
   db_engine_version = "11.8"
   db_identifier = "fss-public-dev-db"
-  db_instance_class = "db.t2.micro"
+  db_instance_class = "db.t3.micro"
   db_name = data.aws_ssm_parameter.fss_public_postgres_database.value
   db_port  = data.aws_ssm_parameter.fss_public_postgres_port.value
   db_username = data.aws_ssm_parameter.fss_public_postgres_username.value
