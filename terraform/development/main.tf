1ecca43f5268d345afc5c8b97725a6a91e907a0b
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2024-09-30 15:51:34 +0100

    Sync the custom database parameter group to the FSS postgres database.
    Co-authored-by: Miles Alford <43789512+Miles-Alford@users.noreply.github.com>

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -73,6 +73,7 @@ module "postgres_db_development" {
   vpc_id = data.aws_vpc.development_vpc.id
   db_engine = "postgres"
   db_engine_version = "16.3"
+  db_parameter_group_name = "postgres-16"
   db_identifier = "fss-public-dev-db"
   db_instance_class = "db.t3.micro"
   db_name = data.aws_ssm_parameter.fss_public_postgres_database.value
