86d32c965f31cfc227b8d868bb26b434dbab1aaa
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2024-09-30 15:50:49 +0100

    Sync the FSS postgres database engine version with the `DevelopmentAPIs` environment.
    Co-authored-by: Miles Alford <43789512+Miles-Alford@users.noreply.github.com>

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -72,7 +72,7 @@ module "postgres_db_development" {
   environment_name = "development"
   vpc_id = data.aws_vpc.development_vpc.id
   db_engine = "postgres"
-  db_engine_version = "11.8"
+  db_engine_version = "16.3"
   db_identifier = "fss-public-dev-db"
   db_instance_class = "db.t3.micro"
   db_name = data.aws_ssm_parameter.fss_public_postgres_database.value
