4bcd4c01a403ce6c5af8218ebfa2399d04be1bee
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2024-10-01 10:54:20 +0100

    Reconfigure the AWS provider and its version for production terraform.
    Co-authored-by: Miles Alford <43789512+Miles-Alford@users.noreply.github.com>

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -7,24 +7,30 @@
 # 6) IF ADDITIONAL RESOURCES ARE REQUIRED BY YOUR API, ADD THEM TO THIS FILE
 # 7) ENSURE THIS FILE IS PLACED WITHIN A 'terraform' FOLDER LOCATED AT THE ROOT PROJECT DIRECTORY
 
-provider "aws" {
-  region  = "eu-west-2"
-  version = "~> 2.0"
-}
-data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
-locals {
-  application_name = "fss public api"
-  parameter_store = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter"
-}
-
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
   backend "s3" {
     bucket  = "terraform-state-production-apis"
     encrypt = true
     region  = "eu-west-2"
     key     = "services/fss-public-api/state"
   }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+locals {
+  application_name = "fss public api"
+  parameter_store = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter"
 }
 
 /*    POSTGRES SET UP    */
